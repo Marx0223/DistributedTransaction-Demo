887545261d94277ef46a7645b570806c825a2539
zeyu.wang <zeyu.wang@zhangmen.com>
2021-03-15 08:51:01 +0800

    rocketmq-demo

---
+++ dtx-parent/dtx-txmsg-rocketmq-demo/dtx-txmsg-demo-bank2/src/main/java/com.marx.dtx.txmsgdemo.bank2/service/AccountInfoService.java
@@ -0,0 +1,12 @@
+package com.marx.dtx.txmsgdemo.bank2.service;
+
+import com.marx.dtx.txmsgdemo.bank2.model.AccountChangeEvent;
+
+/**
+ * Created by Administrator.
+ */
+public interface AccountInfoService {
+
+    //更新账户，增加金额
+    public void addAccountInfoBalance(AccountChangeEvent accountChangeEvent);
+}
